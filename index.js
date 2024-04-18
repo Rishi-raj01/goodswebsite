fa0e67d7102fd3e58ae2f697b6849e4aad803f5d
Rishi-raj01 <114952358+Rishi-raj01@users.noreply.github.com>
2024-04-19 00:27:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(morgan("dev"));
 app.use(cookieParser());
 //app.use(cors());
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "*",
   credentials: true
 }));
 app.get("/", (req, res) => {
